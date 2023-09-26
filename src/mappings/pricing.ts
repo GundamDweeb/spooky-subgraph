1a331f8f6befd38c97c783b63c30bb14ea3d4f74
Kent Theo Fourie <42357713+GundamDweeb@users.noreply.github.com>
2023-09-26 14:08:18 +0200

    fixed pricing

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -41,16 +41,24 @@ export function findEthPerToken(token: Token): BigDecimal {
   }
   // loop through whitelist and check if paired with any
   for (let i = 0; i < WHITELIST.length; ++i) {
-    let pairAddress = factoryContract.getPair(Address.fromString(token.id), Address.fromString(WHITELIST[i]))
-    if (pairAddress.toHexString() != ADDRESS_ZERO) {
-      let pair = Pair.load(pairAddress.toHexString())!
-      if (pair.token0 == token.id && pair.reserveETH.gt(MINIMUM_LIQUIDITY_THRESHOLD_ETH)) {
-        let token1 = Token.load(pair.token1)!
-        return pair.token1Price.times(token1.derivedETH as BigDecimal) // return token1 per our token * Eth per token 1
-      }
-      if (pair.token1 == token.id && pair.reserveETH.gt(MINIMUM_LIQUIDITY_THRESHOLD_ETH)) {
-        let token0 = Token.load(pair.token0)!
-        return pair.token0Price.times(token0.derivedETH as BigDecimal) // return token0 per our token * ETH per token 0
+    let pairAddress = factoryContract.try_getPair(Address.fromString(token.id), Address.fromString(WHITELIST[i]))
+    if(!pairAddress.reverted){
+      if (pairAddress.value.toHexString() != ADDRESS_ZERO) {
+        let pair = Pair.load(pairAddress.value.toHexString())
+        if(pair){
+          if (pair.token0 == token.id && pair.reserveETH.gt(MINIMUM_LIQUIDITY_THRESHOLD_ETH)) {
+            let token1 = Token.load(pair.token1)!
+            if(token1) {
+              return pair.token1Price.times(token1.derivedETH as BigDecimal) // return token1 per our token * Eth per token 1
+            }
+          }
+          if (pair.token1 == token.id && pair.reserveETH.gt(MINIMUM_LIQUIDITY_THRESHOLD_ETH)) {
+            let token0 = Token.load(pair.token0)
+            if(token0) {
+              return pair.token0Price.times(token0.derivedETH as BigDecimal) // return token0 per our token * ETH per token 0
+            }
+          }
+        }
       }
     }
   }
